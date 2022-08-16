45ac383841dbe01fa1dbfc6fba78c79bfd5d68ad
Nikhil Kumar <96546721+nikumar1206@users.noreply.github.com>
2022-08-15 22:45:50 -0400

    Time: 1404 ms (67.44%), Space: 25.1 MB (38.44%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        curr_max = 0
+        global_max = 0
+        buy_date, sell_date = 0, 1
+        while sell_date < len(prices) and (sell_date > buy_date):
+            curr_max = prices[sell_date] - prices[buy_date]
+            if curr_max < global_max and prices[sell_date] < prices[buy_date]:
+                buy_date = sell_date
+            global_max = max(global_max, curr_max)
+            sell_date += 1
+        return global_max
+            
